a0f1a2211fc04a98cad73e782bb0a9ceb33434e6
Michael Bobino <mbobino@gmail.com>
2016-08-05 18:39:48 -0500

    Initial Commit

---
+++ project02/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project02
+//
+//  Created by Robert Bobino on 8/5/16.
+//  Copyright © 2016 bobino.co. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
