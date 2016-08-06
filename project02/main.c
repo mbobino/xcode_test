c4663fb71e59738f5a2b796cf555d9822668f808
Michael Bobino <mbobino@gmail.com>
2016-08-05 20:10:29 -0500

    commit 1 test

--- project02/main.c
+++ project02/main.c
@@ -5,11 +5,26 @@
 //  Created by Robert Bobino on 8/5/16.
 //  Copyright © 2016 bobino.co. All rights reserved.
 //
+//	Convert miles into kilometers
 
 #include <stdio.h>
 
+#define KMS_PER_MILE 1.609
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-    return 0;
+	
+	// Init variables
+	double miles, kms;
+	
+	// Get distance in miles
+	printf("Enter the distance in miles: ");
+	scanf("%lf", &miles);
+	
+	// Convert the distance to kilometers
+	kms = KMS_PER_MILE * miles;
+	
+	// Display the distance in kilometers
+	printf("%f miles equals %f kilometers.\n", miles, kms);
+	
+	return 0;
 }
